3a7729561428630a347cc27a577c5e6fccf71dbe
Zworis <86747548+Zworis@users.noreply.github.com>
2022-05-24 11:29:22 +0200

    Add files via upload

---
+++ Scripts/FollowPlayerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    
+    GameObject plane;
+    GameObject propellor;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        propellor = GameObject.Find("Propeller");
+        plane = GameObject.Find("Player");
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        propellor.transform.Rotate(0,0,40.12443f);
+        transform.position = plane.transform.position + offset;
+    }
+}
