e69bfbfb6c2452b4f43cb4ed37663683a08a57f7
Zworis <86747548+Zworis@users.noreply.github.com>
2022-05-24 11:25:24 +0200

    Add files via upload

---
+++ Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    Vector3 Position;
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Position = transform.position;
+        Player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + Position;
+    }
+}
